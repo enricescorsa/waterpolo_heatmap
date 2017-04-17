a0a80bf6c437dcb567001214e3e223ab5f1426f4
Enric Escorsa O'Callaghan <enricescorsa@users.noreply.github.com>
2017-04-18 00:39:32 +0200

    Update waterpolo_heatmap.r

--- waterpolo_heatmap.r
+++ waterpolo_heatmap.r
@@ -2,18 +2,29 @@
 
 install.packages("d3heatmap")
 install.packages("rvest")
+install.packages("XML")
+
 
 library(rvest)
 classificacio_waterpolo <- html("http://aquatics.cat/competicio/informacioCompeticio/2016/9/49/0")
 
-#recupero la segona taula que apareix en aquesta pagina amb ".[[2]]" que és la que m'interessa i la transformo en un dataframe amb html_table
+#recupero la segona taula que apareix en aquesta pagina amb .[[2]]
 classificacio_waterpolo %>%
   html_nodes("table") %>%
   .[[2]] %>%
   html_table()
 
+#també ho puc fer amb XML (amb "which =2" selecciono la segona taula) i transformarla en un df amb readHTMLTable
+library(XML)
+url <- "http://aquatics.cat/competicio/informacioCompeticio/2016/9/49/0"
+classificacio_waterpolodf <- readHTMLTable (url, which = 2)
+classificacio_waterpolodf
+
 #me la puc descarregar com a csv i després llegir-la
+write.csv(classificacio_waterpolodf, file = "classificacio_waterpolo.csv")
+
+
 #llegeixo el csv de resultats partits de waterpolo des del directori on m'he l'he descarregat
-waterpolo_heatmap <- read.csv("classificacio_waterpolo.csv", row.names = 1)
-library(d3heatmap) #d3heatmap és un paquet per generar heatmaps interactius en D3.js javascript dins R
+waterpolo_heatmap <- read.csv("classificacio_waterpolo.csv", row.names = 3)
+library(d3heatmap) #paquet per generar heatmaps interactius en D3.js javascript dins R
 d3heatmap(waterpolo_heatmap, scale = "column")
